2208dde25410cfc1ecfe9b1856c96e0bd4860d8b
James Vasile <james@jamesvasile.com>
2019-10-03 10:32:14 -0400

    Update for modern React

--- frontend/components/hello.js
+++ frontend/components/hello.js
@@ -1,11 +1,10 @@
 var React = require('react')
 
-module.exports = React.createClass({
-  render: function() {
-    return (
-      <div>
-        <h1>Hello, World!</h1>
-      </div>
-    )
-  }
-})
+class Hello extends React.Component {
+    render() {
+	return (
+		<div>
+		<h1>Hello, World!</h1>
+		</div>
+	);}}
+module.exports = Hello
